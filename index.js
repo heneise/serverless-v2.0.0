314080dc66a057eb600bff7a36ff1bf02e81c0f6
Patrick Heneise <patrick@heneise.co>
2019-06-26 21:23:57 +0300

    fix: standard js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-"use strict";
-
 exports.http = (request, response) => {
-  response.status(200).send({ text: "Hello World!" });
-};
+  response.status(200).send({ text: 'Hello World!' })
+}
 
 exports.event = (event, callback) => {
-  callback();
-};
+  callback()
+}
